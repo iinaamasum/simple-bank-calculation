ad3871678eeb22dd8ea2be8d169bedba4e61cd91
iinaamasum <iinaamasum@gmail.com>
2022-02-11 11:51:29 +0600

    login

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const usereEmail = document.getElementById('floatingInput').value;
+const userePassword = document.getElementById('floatingInput').value;
+
+document.getElementById('submit').addEventListener('click', function(e){
+    e.preventDefault();
+    
+});
