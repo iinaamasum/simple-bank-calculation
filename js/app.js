4dad76080770db9548ef87acf5925dd00e1c632c
iinaamasum <iinaamasum@gmail.com>
2022-02-11 12:08:25 +0600

    login page done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-const usereEmail = document.getElementById('floatingInput').value;
-const userePassword = document.getElementById('floatingInput').value;
-
-document.getElementById('submit').addEventListener('click', function(e){
-    e.preventDefault();
-    
+document.getElementById("submit").addEventListener("click", function (e) {
+  e.preventDefault();
+  const userEmail = document.getElementById("floatingInput").value;
+  const userPassword = document.getElementById("floatingPassword").value;
+  if (userEmail == "" || userPassword == "") {
+    alert("You have entered null vlaue");
+  } else if (userEmail == "iinaamasum@gmail.com" && userPassword == "demo") {
+    window.location.href = "./banking.html";
+  } else {
+    alert("Please Enter valid information");
+  }
+  document.getElementById("floatingInput").value = "";
+  document.getElementById("floatingPassword").value = "";
 });
